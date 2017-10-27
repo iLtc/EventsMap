fab2758fde47f736af343b922bf9b6d052019da7
iLtc <i@iltc.io>
2017-10-26 19:15:29 -0500

    Initial Commit

---
+++ Events Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Events Map
+//
+//  Created by Alan Luo on 10/26/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
