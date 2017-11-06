4d20b5d962869d405810afce00557f63778747e6
iLtc <i@iltc.io>
2017-11-05 22:40:53 -0600

    Change Events Service

--- Events Map/Models/EventModel.swift
+++ Events Map/Models/EventModel.swift
@@ -2,8 +2,33 @@
 //  EventModel.swift
 //  Events Map
 //
-//  Created by Alan Luo on 11/5/17.
+//  Created by Alan Luo on 11/3/17.
 //  Copyright © 2017 The University of Iowa. All rights reserved.
 //
 
 import Foundation
+
+class Event {
+    public var id: String
+    public var title: String
+    public var url: String
+    public var date: String
+    public var location: String
+    public var description: String
+    public var photos: [String] = []
+    public var geo: [String: String] = [:]
+    public var categories: [String] = []
+    
+    init(id: String, title: String, url: String, date: String, location: String, description: String){
+        self.id = id
+        self.title = title
+        self.url = url
+        self.date = date
+        self.location = location
+        self.description = description
+    }
+    
+    func debug() -> String {
+        return "id: \(self.id), title: \(self.title.trunc(7)), url: \(self.url.trunc(7)), date: \(self.date.trunc(7)), location: \(self.location.trunc(7)), description: \(self.description.trunc(21)), photos: \(self.photos.joined(separator: ", ").trunc(7)), geo: \(self.geo), categories: \(self.categories.joined(separator: ", ").trunc(21))"
+    }
+}
--- Events Map/Services/EventService.swift
+++ Events Map/Services/EventService.swift
@@ -2,8 +2,62 @@
 //  EventService.swift
 //  Events Map
 //
-//  Created by Alan Luo on 11/5/17.
+//  Created by Alan Luo on 11/3/17.
 //  Copyright © 2017 The University of Iowa. All rights reserved.
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+
+class EventService {
+    static var instance = EventService()
+    
+    private var events: [ Event ] = []
+    private(set) var isReady = false
+    
+    func sync(_ callback: @escaping (() -> Void)) {
+        Alamofire.request(ConfigService.instance.get("EventsServerHost")).responseJSON { response in
+            if let result = response.result.value {
+                let json = JSON(result)
+                
+                for (_, subJson): (String, JSON) in json {
+                    
+                    let event = Event(
+                        id: subJson["eid"].stringValue,
+                        title: subJson["title"].stringValue,
+                        url: subJson["url"].stringValue,
+                        date: subJson["first_date"].stringValue,
+                        location: subJson["location"].stringValue,
+                        description: subJson["description"].stringValue
+                    )
+                    
+                    /*for (_, photo): (String, JSON) in subJson["photos"] {
+                        print(photo)
+                        event.photos.append(photo.stringValue)
+                    }*/
+                    
+                    event.geo["latitude"] = subJson["geo"]["latitude"].stringValue
+                    event.geo["longitude"] = subJson["geo"]["longitude"].stringValue
+                    
+                    /*for category in subJson["categories"].arrayValue {
+                        event.categories.append(category.stringValue)
+                    }*/
+                    
+                    self.events.append(event)
+                }
+            }
+            
+            self.isReady = true
+            callback()
+        }
+    }
+    
+    func getEvents() -> [Event] {
+        if isReady {
+            return self.events
+        } else {
+            return []
+        }
+    }
+}
