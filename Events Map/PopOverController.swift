420a040677838116d8ebc8d433839cfa80cc9ad1
Yizhen Chen <sharedcare@hotmail.com>
2017-11-12 00:32:45 -0600

    Add PopOverController

--- Events Map/PopOverController.swift
+++ Events Map/PopOverController.swift
@@ -9,5 +9,15 @@
 import UIKit
 
 class PopOverController: UIPresentationController {
-
+    
+    override var frameOfPresentedViewInContainerView: CGRect {
+        var frame: CGRect = .zero
+        frame.size = size(forChildContentContainer: presentedViewController, withParentContainerSize: (containerView?.bounds.size)!)
+        frame.origin.y = containerView!.frame.height*(1.0/4.0)
+        return frame
+    }
+    
+    override init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?) {
+        super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
+    }
 }
