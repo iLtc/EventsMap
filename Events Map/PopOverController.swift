a1036004699da0ac92930c982e6fff3f1be69216
Yizhen Chen <sharedcare@hotmail.com>
2017-11-12 00:30:49 -0600

    Add PopOverController

---
+++ Events Map/PopOverController.swift
@@ -0,0 +1,13 @@
+//
+//  PopOverController.swift
+//  Events Map
+//
+//  Created by uics3 on 11/11/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class PopOverController: UIPresentationController {
+
+}
