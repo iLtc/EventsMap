8b402794ab35e586506b99b2838854f281dcb2f1
Yizhen Chen <sharedcare@hotmail.com>
2017-11-14 00:25:30 -0600

    Add icon images, add MapsCell

---
+++ Events Map/MapsCell.swift
@@ -0,0 +1,13 @@
+//
+//  MapsCell.swift
+//  Events Map
+//
+//  Created by uics3 on 11/13/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class MapsCell: UICollectionViewCell {
+    
+}
