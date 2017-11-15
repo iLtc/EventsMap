8347c11f7f7582c44994e5e58ce7cecd612687f4
iLtc <i@iltc.io>
2017-11-15 17:55:30 -0600

    Add funcation to handle URL Schemes

--- Events Map/AppDelegate.swift
+++ Events Map/AppDelegate.swift
@@ -40,6 +40,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //window?.rootViewController = UIViewController(nibName: "viewController", bundle: nil)
         return true
     }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        if let host = url.host {
+            print(host)
+        }
+        
+        return true
+    }
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
