c1f228762804bcbbd9f7d9fe71f0e966308a912f
iLtc <i@iltc.io>
2017-11-24 14:40:36 -0600

    Refactor

---
+++ Events Map/Others/set-btn-background-color.swift
@@ -0,0 +1,20 @@
+//
+//  set-btn-background-color.swift
+//  Events Map
+//
+//  Created by Alan Luo on 11/24/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func setBackgroundColor(color: UIColor, forState: UIControlState) {
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        UIGraphicsGetCurrentContext()!.setFillColor(color.cgColor)
+        UIGraphicsGetCurrentContext()!.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.setBackgroundImage(colorImage, for: forState)
+    }
+}
