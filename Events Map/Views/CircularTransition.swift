2b84668ee6b7bdb71868ac4a8fde1e72ccd849d0
sharedcare <sharedcare@hotmail.com>
2017-11-25 15:12:40 -0600

    Update Project file

---
+++ Events Map/Views/CircularTransition.swift
@@ -0,0 +1,13 @@
+//
+//  CircularTransition.swift
+//  Events Map
+//
+//  Created by Yizhen Chen on 11/25/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class CircularTransition: NSObject {
+
+}
