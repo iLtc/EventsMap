73b09147735fb01f57a9805385cda43947b618bc
iLtc <i@iltc.io>
2017-11-29 15:57:59 -0600

    Add Loading View

---
+++ Events Map/Others/activity-indicator.swift
@@ -0,0 +1,9 @@
+//
+//  activity-indicator.swift
+//  Events Map
+//
+//  Created by Alan Luo on 11/29/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import Foundation
