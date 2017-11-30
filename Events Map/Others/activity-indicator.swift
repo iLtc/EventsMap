4e250ad1b0c0dc1dc0e2c191f1a0be6e43f8ca86
sharedcare <sharedcare@hotmail.com>
2017-11-30 11:17:42 -0600

    Update indicator

--- Events Map/Others/activity-indicator.swift
+++ Events Map/Others/activity-indicator.swift
@@ -11,6 +11,8 @@ import UIKit
 extension UIViewController {
     // https://stackoverflow.com/questions/28785715/how-to-display-an-activity-indicator-with-text-on-ios-8-with-swift
     func activityIndicator(_ title: String) -> UIView {
+//        let window = UIApplication.shared.keyWindow
+        
         var activityIndicator = UIActivityIndicatorView()
         var strLabel = UILabel()
         let effectView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
@@ -20,7 +22,8 @@ extension UIViewController {
         strLabel.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight.medium)
         strLabel.textColor = UIColor(white: 0.9, alpha: 0.7)
         
-        effectView.frame = CGRect(x: view.frame.midX - strLabel.frame.width/2, y: view.frame.midY - strLabel.frame.height/2 , width: 160, height: 46)
+        effectView.frame = CGRect(x: UIScreen.main.bounds.midX - strLabel.frame.width/2, y: UIScreen.main.bounds.midY - strLabel.frame.height/2 , width: 160, height: 46)
+//        effectView.center = (window?.center)!
         effectView.layer.cornerRadius = 15
         effectView.layer.masksToBounds = true
         
