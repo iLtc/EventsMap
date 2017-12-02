f276246c67f1ed41fb851624a8a2f69913a29276
sharedcare <sharedcare@hotmail.com>
2017-12-02 17:11:09 -0600

    Update project

---
+++ Events Map/Controllers/SearchTextCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchTextCell.swift
+//  Events Map
+//
+//  Created by Yizhen Chen on 12/2/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class SearchTextCell: MDCCollectionViewTextCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
