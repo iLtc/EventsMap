1d147e440a92b5c22ce75d8b190f23e8a85357ee
sharedcare <sharedcare@hotmail.com>
2017-12-02 14:53:29 -0600

    Update Project

---
+++ Events Map/Controllers/SearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchCell.swift
+//  Events Map
+//
+//  Created by Yizhen Chen on 12/2/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
