3b0b9d901e2ab3d2f288bc29a853a2e9c49a2a4e
sharedcare <sharedcare@hotmail.com>
2017-12-03 14:20:48 -0600

    Add liked events list

---
+++ Events Map/Controllers/EventsListCell.swift
@@ -0,0 +1,13 @@
+//
+//  EventsListCell.swift
+//  Events Map
+//
+//  Created by Yizhen Chen on 12/2/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class EventsListCell: MDCCollectionViewCell {
+    
+}
---
+++ Events Map/Controllers/InkOverlay.swift
@@ -0,0 +1,21 @@
+//
+//  InkOverlay.swift
+//  Events Map
+//
+//  Created by Yizhen Chen on 12/3/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class InkOverlay: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
