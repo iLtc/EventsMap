26d0b8586ee73c5bf7d7968a721972a184826d75
iLtc <i@iltc.io>
2017-12-04 17:40:27 -0600

    Add About View

---
+++ Events Map/Controllers/AboutViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AboutViewController.swift
+//  Events Map
+//
+//  Created by Alan Luo on 12/4/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
