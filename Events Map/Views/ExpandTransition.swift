2fc16471f9cc5e3f0745955139c072671ce4f336
sharedcare <sharedcare@hotmail.com>
2017-12-04 17:36:17 -0600

    Add CardDetailVC

---
+++ Events Map/Views/ExpandTransition.swift
@@ -0,0 +1,13 @@
+//
+//  ExpandTransition.swift
+//  Events Map
+//
+//  Created by Yizhen Chen on 12/3/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class ExpandTransition: NSObject {
+
+}
