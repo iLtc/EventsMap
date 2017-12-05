529e945749127af8fde5ecbacf099ea489a26a2c
sharedcare <sharedcare@hotmail.com>
2017-12-04 23:30:37 -0600

    Add new DetailView

---
+++ Events Map/Controllers/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  Events Map
+//
+//  Created by Yizhen Chen on 12/4/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class DetailView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
