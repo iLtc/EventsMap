6941439f1283f577123928da31cda2a52062a2c2
sharedcare <sharedcare@hotmail.com>
2017-12-09 17:24:01 -0600

    Add licences

---
+++ Events Map/Controllers/LicencesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LicencesViewController.swift
+//  Events Map
+//
+//  Created by Yizhen Chen on 12/9/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class LicencesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
