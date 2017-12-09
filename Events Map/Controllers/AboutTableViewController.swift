d8f63294de6cea1890fbaa091ad3adf3a2a179aa
iLtc <i@iltc.io>
2017-12-09 17:48:21 -0600

    Redesign AboutTableViewController

--- Events Map/Controllers/AboutTableViewController.swift
+++ Events Map/Controllers/AboutTableViewController.swift
@@ -21,6 +21,8 @@ class AboutTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.title = "About and Help"
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -38,71 +40,79 @@ class AboutTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        if section == 0 {
+            return 3
+        } else {
+            return 1
+        }
     }
-
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Developer"
+        } else {
+            return "Help"
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 40))
-        cell.textLabel?.text = "Icons"
-        // Configure the cell...
-
-        return cell
+        if indexPath.section == 0 {
+            switch indexPath.row {
+            case 0:
+                let cell = UITableViewCell(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 40))
+                cell.textLabel?.text = "Tiancheng Luo"
+                return cell
+            case 1:
+                let cell = UITableViewCell(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 40))
+                cell.textLabel?.text = "Yizhen Chen"
+                return cell
+            case 2:
+                let cell = UITableViewCell(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 40))
+                cell.textLabel?.text = "Zhenming Wang"
+                return cell
+            default:
+                fatalError("Error row")
+            }
+        } else {
+            let cell = UITableViewCell(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 40))
+            cell.textLabel?.text = "Icons"
+            return cell
+        }
     }
     
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let vc = IconViewController(nibName: nil, bundle: nil)
-        present(vc, animated: true, completion: nil)
+        if indexPath.section == 0 {
+            switch indexPath.row {
+            case 0:
+                openURL(url: URL(string: "https://iLtc.io")!, title: "Yizhen Chen")
+            case 1:
+                openURL(url: URL(string: "https://homepage.divms.uiowa.edu/~ychen261/")!, title: "Yizhen Chen")
+            case 2:
+                openURL(url: URL(string: "https://homepage.divms.uiowa.edu/~zwang191/")!, title: "Zhenming Wang")
+            default:
+                fatalError("Error row")
+            }
+        } else {
+            let vc = IconViewController(nibName: nil, bundle: nil)
+            present(vc, animated: true, completion: nil)
+        }
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func openURL(url: URL, title: String) {
+        let webViewController = WebViewController()
+        webViewController.url = url
+        webViewController.webTitle = title
+        present(webViewController, animated: true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
 }
