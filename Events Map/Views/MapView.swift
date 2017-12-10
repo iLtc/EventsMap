79d14d19df8cfdf0b2c9ba784a6871135460f1fa
sharedcare <sharedcare@hotmail.com>
2017-12-10 14:14:45 -0600

    Add map view

---
+++ Events Map/Views/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  Events Map
+//
+//  Created by Yizhen Chen on 12/9/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class MapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
