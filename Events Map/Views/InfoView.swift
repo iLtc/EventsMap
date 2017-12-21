69019df548506def3240b7f548410cc3afc721de
sharedcare <sharedcare@hotmail.com>
2017-12-21 15:26:34 +0800

    add infoview

---
+++ Events Map/Views/InfoView.swift
@@ -0,0 +1,21 @@
+//
+//  InfoView.swift
+//  Events Map
+//
+//  Created by Yizhen Chen on 12/21/17.
+//  Copyright © 2017 The University of Iowa. All rights reserved.
+//
+
+import UIKit
+
+class InfoView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
