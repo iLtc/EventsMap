c31c6251bcbf1b78f46115060935ac7fd2fb62ac
sharedcare <sharedcare@hotmail.com>
2018-01-19 13:28:16 -0600

    Update email

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 // Configure your Subject Prefix and Recipient here
 $subjectPrefix = '[Contact via HawkEvents]';
-$emailTo       = '<YOUR_EMAIL_HERE>';
+$emailTo       = 'hawkevents@outlook.com';
 $errors = array(); // array to hold validation errors
 $data   = array(); // array to pass back data
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
