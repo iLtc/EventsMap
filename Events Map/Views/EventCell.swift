bae52a03290d02fba96fe81d302a592d85e213ad
sharedcare <sharedcare@hotmail.com>
2018-02-06 15:36:42 -0600

    Add feedback

--- Events Map/Views/EventCell.swift
+++ Events Map/Views/EventCell.swift
@@ -77,16 +77,20 @@ class EventCell: UITableViewCell {
     
     @IBAction func likeBtnPressedDown(_ sender: UIButton) {
         sender.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        let generator = UIImpactFeedbackGenerator(style: .light)
+        generator.impactOccurred()
     }
     
     @IBAction func likeBtnPressedUp(_ sender: UIButton) {
+        
         UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 5, options: .curveEaseOut, animations: {
             sender.transform = CGAffineTransform.identity
         }, completion: nil)
     }
     
     @IBAction func likeBtnPressed(_ sender: UIButton) {
-        
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
         print("like")
         UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 5, options: .curveEaseOut, animations: {
             sender.transform = CGAffineTransform.identity
@@ -100,9 +104,11 @@ class EventCell: UITableViewCell {
                     
                     self.parentVC?.present(alert, animated: true)
                     return
+                } else {
+                    sender.tag = 1
+                    self.event?.liked = false
+                    self.likeBtn.setImage(#imageLiteral(resourceName: "md-star-border"), for: .normal)
                 }
-                sender.tag = 1
-                self.likeBtn.setImage(#imageLiteral(resourceName: "md-star-border"), for: .normal)
             }
         } else if sender.tag == 1 {
             event?.like() { code, msg in
@@ -110,7 +116,7 @@ class EventCell: UITableViewCell {
                 case "200":
                     sender.tag = 0
                     self.likeBtn.setImage(#imageLiteral(resourceName: "md-star"), for: .normal)
-                    
+                    self.event?.liked = true
                 case "400":
                     let alertController = MDCAlertController(title: nil, message: "You need to login.")
                     let cancelAction = MDCAlertAction(title: "Cancel", handler: nil)
